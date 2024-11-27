a4b77288f56a55acfdd93dbecd4b42e2e8e75ddb
alan.dev <alaneth.dev@gmail.com>
2024-11-27 22:28:36 +0800

    modify discv5 encoding.go log level

--- p2p/discover/v5_udp.go
+++ p2p/discover/v5_udp.go
@@ -704,7 +704,7 @@ func (t *UDPv5) handlePacket(rawpacket []byte, fromAddr netip.AddrPort) error {
 			t.unhandled <- up
 			return nil
 		}
-		t.log.Info("Bad discv5 packet", "id", fromID, "addr", addr, "err", err)
+		t.log.Debug("Bad discv5 packet", "id", fromID, "addr", addr, "err", err)
 		return err
 	}
 	if fromNode != nil {
